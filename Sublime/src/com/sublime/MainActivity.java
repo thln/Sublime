bc5bad6d2f681d0105fcde3dcd5b66dd9a2273ef
Tam Henry Le Nguyen <tamnguye@usc.edu>
2014-04-12 14:48:25 -0700

    Fixing hash key, facebook information now comes up

--- Sublime/src/com/sublime/MainActivity.java
+++ Sublime/src/com/sublime/MainActivity.java
@@ -1,59 +1,74 @@
 package com.sublime;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.facebook.*;
 import com.facebook.model.*;
 
-import android.widget.TextView;
-import android.content.Intent;
-
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        // start Facebook Login
-        Session.openActiveSession(this, true, new Session.StatusCallback() {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-          // callback when session changes state
-          @SuppressWarnings("deprecation")
-		@Override
-          public void call(Session session, SessionState state, Exception exception) {
-        	  if (session.isOpened()) {
-        		// make request to the /me API
-        		  Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+    try {
+        //**//be careful to put the correct package name, it was mmy first mistake**
+        PackageInfo info = getPackageManager().getPackageInfo("com.sublime", PackageManager.GET_SIGNATURES);
+        for (Signature signature : info.signatures) {
+            MessageDigest md = MessageDigest.getInstance("SHA");
+            md.update(signature.toByteArray());
+            Log.d("Hash Key:","Hash Key " +Base64.encodeToString(md.digest(), Base64.DEFAULT));
 
-        		    // callback after Graph API response with user object
-        		    @Override
-        		    public void onCompleted(GraphUser user, Response response) {
-        		    	if (user != null) {
-        		    		  TextView welcome = (TextView) findViewById(R.id.welcome);
-        		    		  welcome.setText("Hello " + user.getName() + "!");
-        		    		}
-        		    }
-        		  });
-        	  }
-          }
-        });
-    }
+         }
+         } catch (NameNotFoundException e) {
 
+         } catch (NoSuchAlgorithmException e) {
 
-    //@Override
-    /*public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }*/
+     }    
     
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    
+    // start Facebook Login
+    Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+      // callback when session changes state
+      @Override
+      public void call(Session session, SessionState state, Exception exception) {
+        if (session.isOpened()) {
+
+          // make request to the /me API
+          Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+
+            // callback after Graph API response with user object
+            @Override
+            public void onCompleted(GraphUser user, Response response) {
+              if (user != null) {
+                TextView welcome = (TextView) findViewById(R.id.welcome);
+                welcome.setText("Hello " + user.getName() + "!");
+              }
+            }
+          });
+        }
+      }
+    });
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
       super.onActivityResult(requestCode, resultCode, data);
       Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
-    }
-    
+  }
+
 }
